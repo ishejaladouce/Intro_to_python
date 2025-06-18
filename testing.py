7736983280f27ce216a768f66735476c7ae57a98
ishejaladouce <l.isheja@alustudent.com>
2025-06-18 10:38:10 +0200

    Add identation examples

---
+++ testing.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+#indentation 
+#this is an example of indentation error
+if 5 > 2:
+print("Five is greater than two!")
+
+#this is an example of correct indentation
+if 5 > 2:
+    print("Five is greater than two!")
