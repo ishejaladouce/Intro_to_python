4f2941bf00ad6017e57aafe22b350f061bcef809
ishejaladouce <l.isheja@alustudent.com>
2025-06-23 12:13:59 +0200

    print numbers from 0 to 10 and break the loop when it reaches 10

--- Loops/while-loop.py
+++ Loops/while-loop.py
@@ -2,3 +2,23 @@ count = 0
 while count < 3:
     print ("Hello world")
     count = count + 1
+
+#Example 2: this will print student number from 1 to 10
+count = 0
+while count < 10:
+    print ("Student", count)
+    count += 1
+
+#Example 3: this will print numbers from 0 to 10
+counter = 0
+while counter <=10:
+    print (counter)
+    counter += 1
+
+# Example 4: this will print numbers from 0 to 10 and break the loop when it reaches 10
+counter = 0
+while counter <=10:
+    print (counter)
+    if counter == 10:
+        break
+    counter += 1
