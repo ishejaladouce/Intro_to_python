f19735708383f955464d85c0f73c252da6227654
ishejaladouce <l.isheja@alustudent.com>
2025-06-23 10:22:59 +0200

    Add types of loops and examples for each

---
+++ Loops/for-loop.py
@@ -0,0 +1,11 @@
+# Example 1: this will print "Hello world" 9 times
+for i in range (1, 10):
+    print ("Hello world")
+
+#Example 2": this will print odd numbers from 1 to 9
+for i in range (1, 10, 2):
+    print (i)
+
+#Example 3: this will print "Hello world" 5 Times
+for i in range (1, 10, 2):
+    print ("Hello world")
