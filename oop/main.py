f2f8bfa43836038175cdebbd90a49904fb8af15d
ishejaladouce <l.isheja@alustudent.com>
2025-07-07 10:17:30 +0200

    Add classes and objects in python

---
+++ oop/main.py
@@ -0,0 +1,16 @@
+class Laptops:
+
+    #HardDisk = None
+    #Architecture = None
+    #Model = None
+
+    def __init__(self,HardDisk, Architecture, Model):
+        self.HardDisk = HardDisk
+        self.Architecture = Architecture
+        self.Model = Model
+
+        def TurnOn(self):
+            print("This Laptop can run on by pressing the power button")
+
+        def bits(self):
+            print("This type might support a few software applications")
