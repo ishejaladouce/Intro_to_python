0d2c65d122e20ecad4246d6b087dbe66fd9bdb65
ishejaladouce <l.isheja@alustudent.com>
2025-07-09 11:43:19 +0200

    feat: implement inheritance in OOP examples to extend base class functionality

--- oop/Airline.py
+++ oop/Airline.py
@@ -6,14 +6,23 @@ class AirlineTicket:
         self.passenger_name = passenger_Name
         self.weight = weight
 
+#Add inheritance to the AirlineTicket class
+# The Booking class will inherit from the AirlineTicket class
+Flight_number = input("Enter flight number: ")
+class Booking (AirlineTicket):
+    def __init__(self,passenger_Name, weight, Flight_number):
+        self.Flight_number = Flight_number
+        super().__init__(passenger_Name, weight)
+
+# Define a method to book a ticket that checks the weight limit
     def book_ticket(self):
         if self.weight <= 350:
-            print(f"Dear, {self.passenger_name} you can board the flight")
+            print(f"Dear, {self.passenger_name}, with flight number {self.Flight_number} you can board the flight")
 
         else:
-            print(f"Dear, {self.passenger_name} you cannot board the flight due to excess weight.")
+            print(f"Dear, {self.passenger_name}, with flight number {self.Flight_number} you cannot board the flight due to excess weight.")
 
 # Instantiate the AirlineTicket class and book a ticket
-ticket = AirlineTicket(passenger_Name, Weight)
+ticket = Booking(passenger_Name, Weight, Flight_number)
 
 ticket.book_ticket()
